3a6828c72b835ce0fabab685488a5d2f0cd5accc
ArpanaMishra94 <arpanamishra394@gmail.com>
2022-11-30 12:26:08 +0530

    create and update Category

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -1,44 +1,51 @@
 import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
 import EditSharpIcon from "@mui/icons-material/EditSharp";
+import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import Container from "@mui/material/Container";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
+import Cookies from "js-cookie";
 import * as React from "react";
-import Cookies from 'js-cookie';
-import { useSelector, useDispatch } from "react-redux";
-import {setUser} from '../store/auth.js';
+import { useDispatch, useSelector } from "react-redux";
+import CategoryForm from "../components/CategoryForm.js";
+import { setUser } from "../store/auth.js";
 
 export default function Category() {
-  const user = useSelector(state => state.auth.user)
-  const token = Cookies.get('token');
+  const token = Cookies.get("token");
+  const user = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
+  const [editCategory, setEditCategory] = React.useState({});
+
+  function setEdit(category) {
+    setEditCategory(category);
+  }
 
   async function remove(id) {
     const res = await fetch(`${process.env.REACT_APP_API_URL}/category/${id}`, {
-        method: 'DELETE',
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
 
-    if(res.ok) {
+    if (res.ok) {
       const _user = {
         ...user,
-        categories: user.categories.filter((cat) => cat._id !==   id)
-      }
-      dispatch(setUser({user:_user}))
+        categories: user.categories.filter((cat) => cat._id != id),
+      };
+      dispatch(setUser({ user: _user }));
     }
   }
 
   return (
     <Container>
+      <CategoryForm editCategory={editCategory} />
       <Typography sx={{ marginTop: 10 }} variant="h6">
         List of Categories
       </Typography>
@@ -65,7 +72,7 @@ export default function Category() {
                   <IconButton
                     color="primary"
                     component="label"
-                    // onClick={() => setEditTransaction(row)}
+                    onClick={() => setEdit(row)}
                   >
                     <EditSharpIcon />
                   </IconButton>
--- server/controller/CategoryController.js
+++ server/controller/CategoryController.js
@@ -5,4 +5,33 @@ export const destroy = async(req, res) => {
   const newCategories = categories.filter((category) => category._id != req.params.id);
   const user = await User.updateOne({_id: req.user.id}, {$set: {categories: newCategories}})
   res.json({ user });
-  } 
+}
+
+export const create = async (req, res) => {
+  const {label, icon} = req.body
+  const response = await User.updateOne(
+    {_id: req.user.id},
+    {$set: {categories: [...req.user.categories, {label, icon}]}}
+  );
+
+  res.json({response})
+};
+
+export const update = async (req, res) => {
+  const { label, icon } = req.body;
+  const response = await User.updateOne(
+    { _id: req.user.id },
+    {
+      $set: {
+        categories: req.user.categories.map((category) => {
+          if (category._id == req.params.id) {
+            return { label, icon };
+          }
+          return category;
+        }),
+      },
+    }
+  );
+
+  res.json({ response });
+};
